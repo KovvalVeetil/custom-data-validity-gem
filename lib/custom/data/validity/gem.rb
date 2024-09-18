16b71d9016a4931539ce0f3109c48168881f69a7
Aswathi <akv2595@gmail.com>
2024-09-18 15:28:44 -0400

    gem structure readme

---
+++ lib/custom/data/validity/gem.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "gem/version"
+
+module Custom
+  module Data
+    module Validity
+      module Gem
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/custom/data/validity/gem/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Custom
+  module Data
+    module Validity
+      module Gem
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
